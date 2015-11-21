861a238eade0a6e9847c4a0940316b5e7cbc54f8
Mithi <mithi98@hotmail.com>
2015-11-21 23:17:06 +0800

    Added teacher model and table

---
+++ db/migrate/20151121111120_create_teachers.rb
@@ -0,0 +1,16 @@
+require_relative '../config'
+
+# this is where you should use an ActiveRecord migration to 
+
+class CreateTeachers < ActiveRecord::Migration
+  def change
+    create_table :teachers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      
+      t.timestamps null: false
+    end
+  end
+end
